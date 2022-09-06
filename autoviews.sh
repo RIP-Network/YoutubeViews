1988cafbbe8ebbbdb5f2c2faa45c33034829c1f4
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-09-06 17:13:34 +0000

    Add files via upload

---
+++ autoviews.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+
+echo "_____.___.              __       ___.           "
+echo "\__  |   | ____  __ ___/  |_ __ _\_ |__   ____  "
+echo " /   |   |/  _ \|  |  \   __\  |  \ __ \_/ __ \ "
+echo " \____   (  <_> )  |  /|  | |  |  / \_\ \  ___/ "
+echo " / ______|\____/|____/ |__| |____/|___  /\___  >"
+echo " \/                                   \/     \/ "
+echo "      .__                     "
+echo "___  _|__| ______  _  ________"
+echo "\  \/ /  |/ __ \ \/ \/ /  ___/"
+echo " \   /|  \  ___/\     /\___ \ "
+echo "  \_/ |__|\___  >\/\_//____  >"
+echo "              \/           \/ "
+echo "Creado por RIP-Network          Version 1.0"
+echo 
+echo 
+read -p "Ponga el enlace del video : (ejem https://www.youtube.com/watch?v=fTAiX_Rx4Dw ):" $enlace
+open $enlace
+open $enlace
+open $enlace
+open $enlace
